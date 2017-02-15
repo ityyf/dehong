d6c338c2b01209650b344f36df77c0fdbf9b69d8
yifeng <13366580495@163.com>
2017-02-15 09:04:54 +0800

    modified .idea files

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Admin;
 header('content-type:text/html;charset=utf-8');
 
+use App\Http\Controllers\Controller;
 use Request;
 use DB;
 use Symfony\Component\HttpFoundation\Session\Session;
-class IndexController extends CommonController
+class IndexController extends Controller
 {
 
     public function index(){
